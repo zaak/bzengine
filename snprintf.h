86418f24555b0e13221d8e97f21c24902614dbf8
Pawel Wiaderny <pawel.wiaderny@gmail.com>
2016-02-14 16:23:53 +0100

    Move strlcpy form header file

--- snprintf.h
+++ snprintf.h
@@ -74,33 +74,6 @@ Example:
 
 #include "zend.h"
 
-PHPAPI size_t strlcpy(dst, src, siz)
-		char *dst;
-		const char *src;
-		size_t siz;
-{
-	register char *d = dst;
-	register const char *s = src;
-	register size_t n = siz;
-
-	/* Copy as many bytes as will fit */
-	if (n != 0 && --n != 0) {
-		do {
-			if ((*d++ = *s++) == 0)
-				break;
-		} while (--n != 0);
-	}
-
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';		/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
-
-	return(s - src - 1);	/* count does not include NUL */
-}
 
 typedef int bool_int;
 
